41e43332453d17b14296b221a5567bf511ff75d3
jipenalver <jianchris2k14@gmail.com>
2024-12-09 01:34:06 +0800

    feat: updated login page

--- src/components/layout/AppLayout.vue
+++ src/components/layout/AppLayout.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
+import imgTopNav from '@/assets/images/img-top-nav.png'
+import { useDisplay } from 'vuetify'
 import { ref } from 'vue'
+
+const { mobile } = useDisplay()
 
 const theme = ref('light')
 
@@ -11,12 +15,18 @@ function onClick() {
 <template>
   <v-responsive>
     <v-app :theme="theme">
-      <v-app-bar class="px-3">
+      <v-app-bar theme="dark" :image="imgTopNav" class="px-3">
+        <v-app-bar-nav-icon icon="mdi-menu" :theme="theme"> </v-app-bar-nav-icon>
+
+        <v-app-bar-title>
+          <span class="font-weight-black">SIAM Analytics</span>
+        </v-app-bar-title>
+
         <v-spacer></v-spacer>
 
         <v-btn
-          :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
-          text="Toggle Theme"
+          :icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
+          variant="elevated"
           slim
           @click="onClick"
         ></v-btn>
@@ -25,6 +35,12 @@ function onClick() {
       <v-main>
         <slot name="content"></slot>
       </v-main>
+
+      <v-footer border app>
+        <span class="font-weight-bold" :class="mobile ? 'text-caption' : ''">
+          Copyright © 2024 - SIAM - CSU | All Rights Reserved
+        </span>
+      </v-footer>
     </v-app>
   </v-responsive>
 </template>
---
+++ src/views/auth/login/LoginView.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import AppLayout from '@/components/layout/AppLayout.vue'
+import bgCsu from '@/assets/images/bg-csu.png'
+import LoginForm from './partials/LoginForm.vue'
+</script>
+
+<template>
+  <AppLayout>
+    <template #content>
+      <v-img :src="bgCsu" class="h-screen" cover>
+        <v-container fluid>
+          <v-row>
+            <v-col cols="12">
+              <LoginForm></LoginForm>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-img>
+    </template>
+  </AppLayout>
+</template>
