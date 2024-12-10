de1dc643c711bc338cf371e406ef2bc92c660f46
jipenalver <jianchris2k14@gmail.com>
2024-12-10 17:58:56 +0800

    fix: converted bar to line graph

--- src/views/system/dashboard/partials/StudentGraph.vue
+++ src/views/system/dashboard/partials/StudentGraph.vue
@@ -1,22 +1,44 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+// Categories
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const lastSixMonths = ['July', 'August', 'September', 'October', 'November', 'December']
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const lastTwelveMonths = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+]
+const last7Days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 
-const themeVal = ref('')
-
-const barOptions = {
+// Options
+const options = {
   chart: {
     height: 350,
-    type: 'bar',
+    type: 'line',
     toolbar: {
       show: false,
+    },
+    zoom: {
+      enabled: false,
     },
   },
   colors: ['#2E93fA', '#66DA26', '#546E7A', '#E91E63', '#FF9800'],
   plotOptions: {
-    bar: {
-      columnWidth: '45%',
-      distributed: true,
+    line: {
+      isSlopeChart: false,
     },
+  },
+  stroke: {
+    curve: 'straight',
   },
   dataLabels: {
     enabled: true,
@@ -29,16 +51,7 @@ const barOptions = {
   },
   xaxis: {
     type: 'category',
-    categories: [
-      ['John', 'Doe'],
-      ['Joe', 'Smith'],
-      ['Jake', 'Williams'],
-      'Amber',
-      ['Peter', 'Brown'],
-      ['Mary', 'Evans'],
-      ['David', 'Wilson'],
-      ['Lily', 'Roberts'],
-    ],
+    categories: last7Days,
     labels: {
       style: {
         colors: '#C62828',
@@ -47,7 +60,7 @@ const barOptions = {
       },
     },
     title: {
-      text: 'Products',
+      text: 'Students',
       style: {
         color: '#C62828',
       },
@@ -62,32 +75,48 @@ const barOptions = {
       },
     },
     title: {
-      text: 'Quantity',
+      text: 'Occurence',
       style: {
         color: '#C62828',
       },
     },
   },
-  theme: {
-    mode: themeVal.value,
-  },
-  tooltip: {
-    theme: themeVal.value,
-  },
 }
 
 const series = [
   {
-    name: 'Quantity',
-    data: [21, 22, 10, 28, 16, 21, 13, 30],
+    name: 'Student 1',
+    data: [21, 22, 10, 28, 16, 21, 13],
+  },
+  {
+    name: 'Student 2',
+    data: [1, 22, 10, 2, 16, 21, 13],
+  },
+  {
+    name: 'Student 3',
+    data: [1, 0, 0, 0, 0, 21, 13],
   },
 ]
 </script>
 
 <template>
-  <v-card title="Student Ideation Graph" subtitle="Occurence in the Last 30 days">
+  <v-card title="Student Ideation Graph" subtitle="Occurence in the Last 7 days">
+    <template #append>
+      <v-select
+        min-width="250px"
+        prepend-inner-icon="mdi-calendar"
+        :items="['Last 7 Days', 'Last 14 Days', 'Last 4 Weeks', 'Last 8 Weeks', 'Last 6 Months']"
+        density="compact"
+        label="Filter"
+        item-title="name"
+        item-value="id"
+        variant="outlined"
+        clearable
+      ></v-select>
+    </template>
+
     <v-card-text>
-      <apexchart type="bar" width="100%" :options="barOptions" :series="series"></apexchart>
+      <apexchart type="line" width="100%" :options="options" :series="series"></apexchart>
     </v-card-text>
   </v-card>
 </template>
